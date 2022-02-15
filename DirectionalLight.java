5ae64412da7cdac9cbb63821ffa5b046ed0e4eef
ph04 <47356461+ph04@users.noreply.github.com>
2022-02-15 19:14:04 +0100

    Delete DirectionalLight.java

--- DirectionalLight.java
+++
@@ -1,9 +0,0 @@
-public class DirectionalLight {
-    public double intensity;
-    public Vector3D direction;
-
-    public DirectionalLight(double intensity, Vector3D direction) {
-        this.intensity = intensity;
-        this.direction = direction;
-    }
-}
