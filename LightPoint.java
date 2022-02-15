fe2555395996e34e43b3d61ef27c669ee8eec9a4
ph04 <47356461+ph04@users.noreply.github.com>
2022-02-15 19:14:21 +0100

    Delete LightPoint.java

--- LightPoint.java
+++
@@ -1,9 +0,0 @@
-public class LightPoint {
-    public double intensity;
-    public Point3D position;
-
-    public LightPoint(double intensity, Point3D position) {
-        this.intensity = intensity;
-        this.position = position;
-    }
-}
