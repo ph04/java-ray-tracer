f142671651a7e68a4bcf242f990424b8bbd033df
ph04 <47356461+ph04@users.noreply.github.com>
2022-02-15 19:14:27 +0100

    Delete Ray.java

--- Ray.java
+++
@@ -1,9 +0,0 @@
-public class Ray {
-    public Point3D origin;
-    public Vector3D direction;
-
-    public Ray(Point3D origin, Vector3D direction) {
-        this.origin = origin;
-        this.direction = direction;
-    }
-}
